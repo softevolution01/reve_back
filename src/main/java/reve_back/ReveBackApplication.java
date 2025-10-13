156c3d2c1360ad32d4d531cc8228527c9f8ac011
MarvinAchas <marvinacha@ghlperu.com>
2025-10-13 11:11:21 -0500

    feat: creacion de arquitectura hexagonal

---
+++ src/main/java/reve_back/ReveBackApplication.java
@@ -0,0 +1,13 @@
+package reve_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReveBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReveBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/reve_back/ReveBackApplicationTests.java
@@ -0,0 +1,13 @@
+package reve_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReveBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
