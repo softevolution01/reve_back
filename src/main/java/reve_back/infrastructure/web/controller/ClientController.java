0be72b0a7fbde3d45c0d64c221bfcc0c7801f626
MarvinAchas <marvinacha@ghlperu.com>
2025-12-08 21:43:08 -0500

    fix: cambiando nombre de ruta al controlador de client

--- src/main/java/reve_back/infrastructure/web/controller/ClientController.java
+++ src/main/java/reve_back/infrastructure/web/controller/ClientController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/clients")
+@RequestMapping("/api/clients")
 public class ClientController {
 
     private final SearchClientUseCase searchClientUseCase;
