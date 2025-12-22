b0ffcc26c56600f82e5daeb921b956d8aef41287
MarvinAchas <marvinacha@ghlperu.com>
2025-12-22 16:14:48 -0500

    fix: correncion en el tipo de dato global

--- src/main/java/reve_back/infrastructure/web/dto/SaleItemRequest.java
+++ src/main/java/reve_back/infrastructure/web/dto/SaleItemRequest.java
@@ -3,12 +3,12 @@ package reve_back.infrastructure.web.dto;
 import java.math.BigDecimal;
 
 public record SaleItemRequest(
-        Long idInventario, // Puede ser ID de Product O de DecantPrice
-        String tipoVendible, // "Botella" o "Decant"
+        Long idInventario,
+        String tipoVendible,
         Integer quantity,
-        BigDecimal price, // Precio unitario al momento de la venta
+        BigDecimal price,
 
-        // Lógica de "Cubetas"
-        Boolean forcePromo, // True si el vendedor fuerza la entrada
-        Long forcePromoId   // ID de la promo específica si hay conflicto (Opcional)
+        BigDecimal manualDiscount,
+        Boolean forcePromo,
+        Long forcePromoId
 ) {}
