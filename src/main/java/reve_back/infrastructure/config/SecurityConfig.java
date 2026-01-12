6a35f482fac5c778f1e016c5e2b72c4e064bca14
Tefa <estefany.alfaro.rz@gmail.com>
2026-01-12 08:31:58 -0500

    entorno local

--- src/main/java/reve_back/infrastructure/config/SecurityConfig.java
+++ src/main/java/reve_back/infrastructure/config/SecurityConfig.java
@@ -104,7 +104,8 @@ public class SecurityConfig {
         config.setAllowedOrigins(Arrays.asList(
                 "https://reve-front.vercel.app",
                 "http://localhost:5173",
-                "http://localhost:3000",
+                "http://localhost:4000",
+                "http://localhost:8081",
                 "https://reveback-production.up.railway.app"
         ));
 
